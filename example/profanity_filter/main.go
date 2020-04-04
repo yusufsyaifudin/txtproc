809835cf341d515759eb12e02a26bef96427de89
Yusuf Syaifudin <yusuf.syaifudin@gmail.com>
2020-04-04 10:10:38 +0700

    using DIY comparer example with whitelisted word

--- example/profanity_filter/main.go
+++ example/profanity_filter/main.go
@@ -7,8 +7,13 @@ import (
 	"ysf/txtproc/similarity"
 )
 
+type badWord struct {
+	str            string
+	strReplacement string
+}
+
 type repo struct {
-	badWordsWithPage map[int64]map[string]string
+	badWordsWithPage map[int64]*badWord
 }
 
 func (r repo) Get(_ context.Context, dataNumber int64) (dataReplacer *txtproc.ReplacerData, err error) {
@@ -17,15 +22,10 @@ func (r repo) Get(_ context.Context, dataNumber int64) (dataReplacer *txtproc.Re
 		return &txtproc.ReplacerData{}, nil
 	}
 
-	for k, v := range m {
-		fmt.Println(k, v)
-		return &txtproc.ReplacerData{
-			StringToCompare:   k,
-			StringReplacement: v,
-		}, nil
-	}
-
-	return &txtproc.ReplacerData{}, nil
+	return &txtproc.ReplacerData{
+		StringToCompare:   m.str,
+		StringReplacement: m.strReplacement,
+	}, nil
 }
 
 func (r repo) Total(_ context.Context) int64 {
@@ -33,13 +33,14 @@ func (r repo) Total(_ context.Context) int64 {
 }
 
 func newRepos(data map[string]string) txtproc.ReplacerDataSeeder {
-	var badWordsWithPage = make(map[int64]map[string]string)
+	var badWordsWithPage = make(map[int64]*badWord)
 	var idx = int64(0)
 	for k, v := range data {
 		idx++
 
-		badWordsWithPage[idx] = map[string]string{
-			k: v,
+		badWordsWithPage[idx] = &badWord{
+			str:            k,
+			strReplacement: v,
 		}
 	}
 
@@ -48,22 +49,58 @@ func newRepos(data map[string]string) txtproc.ReplacerDataSeeder {
 	}
 }
 
+type comparer struct {
+	whitelist map[string]bool
+}
+
+// Compare will compare the str1 and str2.
+// str1 is always current word to compare while str2 is the word from db.
+// When we have some list of whitelisted words, we can always compare with str1
+func (c comparer) Compare(_ context.Context, str1 string, str2 string) (score float64, err error) {
+	// if word is in whitelist, then skip to replace
+	if ok, exist := c.whitelist[str1]; ok && exist {
+		return 0, nil
+	}
+
+	if str1 == str2 {
+		return 1, nil
+	}
+
+	return 0, nil
+}
+
+func newComparerWithWhitelistedWords(whiteList []string) similarity.Similarity {
+	var whiteListMap = make(map[string]bool)
+	for _, w := range whiteList {
+		whiteListMap[w] = true
+	}
+
+	return &comparer{
+		whitelist: whiteListMap,
+	}
+}
+
 func main() {
 
 	replacerData := newRepos(map[string]string{
 		"anjing": "*nj*ng",
 		"asu":    "***",
+		"kirik":  "***",
+	})
+
+	simAlg := newComparerWithWhitelistedWords([]string{
+		"kirik",
 	})
 
 	config := txtproc.ReplacerConfig{
 		WordToCompare: txtproc.WordNormalized,
 		ReplacerData:  replacerData,
 		CaseSensitive: false,
-		Similarity:    similarity.ExactMatcher(),
+		Similarity:    simAlg,
 		MinimumScore:  1,
 	}
 
-	text := "Dasar anjing asu!"
+	text := "Dasar anjing asu! kirik"
 	words, _ := txtproc.WordReplacer(context.Background(), text, config)
 	fmt.Println(words.GetOriginalText())
 	fmt.Println(words.GetNormalizedText())
--- similarity/exact_matcher.go
+++ similarity/exact_matcher.go
@@ -8,6 +8,7 @@ import (
 
 type exactMatcher struct{}
 
+// Compare return 1 when str1 == str2, otherwise 0
 func (e exactMatcher) Compare(ctx context.Context, str1 string, str2 string) (score float64, err error) {
 	span, ctx := opentracing.StartSpanFromContext(ctx, "similarity.Compare")
 	defer func() {
