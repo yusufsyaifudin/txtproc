79c713d4c2a7dc907f9e945c44e5fa2008be54b8
Yusuf Syaifudin <yusuf.syaifudin@gmail.com>
2020-10-23 14:22:21 +0700

    add locker on words

--- word/word.go
+++ word/word.go
@@ -1,7 +1,12 @@
 package word
 
+import (
+	"sync"
+)
+
 // Word is a struct represent the word
 type Word struct {
+	lock       sync.RWMutex
 	original   string
 	normalized string
 	replaced   string
@@ -10,18 +15,25 @@ type Word struct {
 
 // GetOriginal get original string
 func (m *Word) GetOriginal() string {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
 	return m.original
 }
 
 // GetNormalized get normalized string, it will contain alphanumeric character only
 func (m *Word) GetNormalized() string {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
 	return m.normalized
 }
 
 // GetReplaced get string that already replaced, will same as GetOriginal when needed
 func (m *Word) GetReplaced() string {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
 	if m.replaced == "" {
-		return m.GetOriginal()
+		return m.original
 	}
 
 	return m.replaced
@@ -29,12 +41,16 @@ func (m *Word) GetReplaced() string {
 
 // SetReplaced replaces the string in GetReplaced
 func (m *Word) SetReplaced(str string) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
 	m.replaced = str
 	m.isReplaced = true
 }
 
 // IsReplaced whether the current string already replaced or not
 func (m *Word) IsReplaced() bool {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
 	return m.isReplaced
 }
 
